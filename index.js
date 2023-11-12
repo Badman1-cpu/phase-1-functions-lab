1580eb6051cf7e16832806d21b08185ef55300c3
Tayler <taylerblevins@gmail.com>
2023-11-12 18:02:56 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    const blocks =42 - location;
+    return Math.abs(blocks);
+}
+function distanceFromHqInFeet(feet){
+    let a = (distanceFromHqInBlocks(feet)) * 264;
+    return a 
+}
+function distanceTravelledInFeet(start,destination){
+    const b = Math.abs(start - destination);
+    const c = b*264;
+    return  c
+}
+function calculatesFarePrice(start,destination){
+    const d = distanceTravelledInFeet(start,destination);
+    if (d <= 400){
+        return d*0
+    }
+    else if (d > 400 && d< 2000){
+        return ((d - 400)* 0.02)
+    }
+    else if (d>2000 && d<2500){
+        return 25
+    }
+    else if (d > 2500){
+        return 'cannot travel that far'
+    }
+
+}
